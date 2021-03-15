30bc255d026d175001753ff1247b1ea037077a28
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-03-15 13:00:58 -0300

    Adicionado reducer para palavra secreta e testes relacionados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React from 'react';
+import hookActions from './actions/hookActions';
 import './App.css';
 import { Congrats } from './components/Congrats/Congrats';
 
 import { GuessedWords } from './components/GuessedWords/GuessedWords'
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload }
+    default:
+      throw new Error(`Invalid action type ${action.type}`)
+  }
+}
+
 function App() {
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    { secretWord: null }
+  )
+
+  const setSecretWord = (secretWord) =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(
+    () => { hookActions.getSecretWord(setSecretWord) },
+    []
+  )
   return (
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,35 @@
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
+import hookActions from './actions/hookActions'
+
+
+const mockGetSecretWord = jest.fn();
 
 const setup = () => {
-    return shallow(<App />);
+    mockGetSecretWord.mockClear();
+    hookActions.getSecretWord = mockGetSecretWord;
+    return mount(<App />);
 }
 
 test('App renders without error', () => {
     const wrapper = setup();
     const component = findByTestAttr(wrapper, 'component-app');
     expect(component.length).toBe(1);
+})
+
+describe('getSecretWord calls', () => {
+    test('getSecretWord gets called on App mount', () => {
+        setup();
+
+        expect(mockGetSecretWord).toHaveBeenCalled();
+    })
+    test('secretWord does not update on App update', () => {
+        const wrapper = setup();
+        mockGetSecretWord.mockClear();
+        //Atualiza componente.
+        wrapper.setProps();
+        expect(mockGetSecretWord).not.toHaveBeenCalled();
+    })
 })
