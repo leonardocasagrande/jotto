6aaac0ba3db489915d2629f9228a1d8f0da8182a
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-03-14 21:46:02 -0300

    Iniciado testes e componente de GuessedWords

---
+++ src/components/GuessedWords/GuessedWords.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+export function GuessedWords(props) {
+    let contents
+    if (props.guessedWords.length === 0) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        )
+    }
+    return (
+        <div data-test="component-guessed-word">
+            {contents}
+        </div>
+    )
+}
+
+GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string.isRequired,
+            letterMatchCount: PropTypes.number.isRequired
+        })
+    ).isRequired
+}
